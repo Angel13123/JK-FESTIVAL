812b9211cfe5645bda59016a7d83cf3f86d65910
Angel Hernandez Epifanio <angelhernandezepifanio@gmail.com>
2025-11-09 04:46:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3651782142-f674a",
+  "appId": "1:384049341605:web:6436564a6a117e7a74194c",
+  "apiKey": "AIzaSyCbHfL5xJWXLSZSEnEgQf5UznunVeax0JI",
+  "authDomain": "studio-3651782142-f674a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "384049341605"
+};
