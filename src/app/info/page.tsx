7f917a64114e0f01a70a3358259618127f9edb27
Angel Hernandez Epifanio <angelhernandezepifanio@gmail.com>
2025-11-26 02:49:03 +0000

    sin sombras por favor

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -70,11 +70,11 @@ export default function InfoPage() {
             </CardHeader>
             <CardContent className="space-y-4 text-muted-foreground">
                 <div>
-                    <h3 className="font-bold text-lg text-foreground flex items-center gap-2"><Car className="h-4 w-4"/>En Coche</h3>
+                    <h3 className="font-bold text-lg text-black flex items-center gap-2" style={{textShadow: 'none'}}><Car className="h-4 w-4"/>En Coche</h3>
                     <p>Habrá un parking habilitado cerca del recinto. Se recomienda compartir coche y llegar con antelación.</p>
                 </div>
                  <div>
-                    <h3 className="font-bold text-lg text-foreground flex items-center gap-2"><Bus className="h-4 w-4"/>Transporte Público</h3>
+                    <h3 className="font-bold text-lg text-black flex items-center gap-2" style={{textShadow: 'none'}}><Bus className="h-4 w-4"/>Transporte Público</h3>
                     <p>Se reforzarán las líneas de autobús desde Tetuán y otras ciudades cercanas. Consulta los horarios especiales que se publicarán próximamente.</p>
                 </div>
             </CardContent>
