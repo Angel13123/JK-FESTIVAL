1cb132a7c5668695c8306065c37c26e3f8d7890e
Angel Hernandez Epifanio <angelhernandezepifanio@gmail.com>
2025-11-26 02:27:14 +0000

    borra estas olas blancas (_for element use_)

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -30,7 +30,7 @@ export function Hero() {
 
   return (
     <section 
-      className="relative w-full min-h-screen overflow-hidden flex flex-col items-center justify-center p-4 bg-transparent"
+      className="relative w-full min-h-[calc(100vh-80px)] overflow-hidden flex flex-col items-center justify-center p-4 bg-transparent"
     >
         
         <div className="relative z-10 flex-grow h-full flex flex-col items-center justify-center gap-2 text-center">
@@ -67,24 +67,6 @@ export function Hero() {
               </Button>
             </div>
         </div>
-
-        <div className="absolute bottom-0 left-0 w-full">
-            <div>
-                <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
-                viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
-                <defs>
-                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                </defs>
-                <g className="parallax">
-                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-                <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
-                </g>
-                </svg>
-            </div>
-        </div>
-
     </section>
   );
 }
