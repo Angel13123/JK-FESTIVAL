54ddded4c078f70aefb1e1bcad3dbaa1f86ca934
Angel Hernandez Epifanio <angelhernandezepifanio@gmail.com>
2025-12-09 02:55:31 +0000

    1. 📝 Aplicar la Corrección en Firebase Studio Tienes que editar el comp

---
+++ src/app/next.config.ts
@@ -0,0 +1,49 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
